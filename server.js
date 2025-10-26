b1eb2a23855182015377aba510128a78f9b63cde
Nguyễn Quang Liêm <liemdl2001@gmail.com>
2025-10-26 23:22:47 +0700

    Cập nhạt app.listen

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`--> Server đang chạy tại http://localhost:${PORT}`);
 });
+
