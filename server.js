4448f5a415a4fc575961cd112234777bb297ee76
fooddu <130214027+fooddu@users.noreply.github.com>
2025-11-01 22:41:34 +0700

    Lưu tạm công việc trên server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, message: err.message });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.POƯRT || 5000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`--> Server đang chạy tại http://localhost:${PORT}`);
